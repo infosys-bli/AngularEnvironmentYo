afe5744b6b29cabed0998770cbee0e0794790ab6
vinay bhutada <vinay_bhutada@infosys.com>
2015-07-30 17:39:02 +0530

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoWorkspaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoWorkspaceApp
+ */
+angular.module('yoWorkspaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoWorkspaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoWorkspaceApp
+ */
+angular.module('yoWorkspaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
